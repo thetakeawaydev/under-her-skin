cd302f217d10f63979de331478150af34a6e7ee7
Tyler Adams <tyleraadams@gmail.com>
2016-10-12 17:19:01 -0400

    smaller assets for mobile

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -4,7 +4,14 @@ import './Hero.css';
 const Hero = () => {
 	return (
 		<header>
-			<img src="UnderHerSkin-opt.png" className="hero__poster" />
+			<picture>
+				<source media="(max-width: 40em)"
+				sizes="100vw"
+				srcSet="UnderHerSkin-mobile-opt.png 810w"
+				/>
+				<img src="UnderHerSkin-opt.png"
+				className="hero__poster" />
+			</picture>
 		</header>
 	);
 }
