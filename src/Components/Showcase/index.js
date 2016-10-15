a6a2faa81c1a23ee58d8f8685e999d921f0d8d94
Tyler Adams <tyleraadams@gmail.com>
2016-10-15 15:41:39 -0400

    [MISC] protocoless resourcess

--- src/Components/Showcase/index.js
+++ src/Components/Showcase/index.js
@@ -4,7 +4,7 @@ import AudioPlayer from '../AudioPlayer';
 
 const Showcase = (props) => {
 	const { audio, timelineId, onAudioClick } = props;
-	const timeline = `http://cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=${props.timelineId}&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650`;
+	const timeline = `//cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=${props.timelineId}&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650`;
 	return (
 		<section className="showcase">
 			<AudioPlayer audio={audio} />
